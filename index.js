da074a54563938e0bfcae2ece19ba736336ae819
vidyasagar <vidyasagar.abburi@yahoo.com>
2023-08-14 06:43:08 +0000

    dateafterxdays

---
+++ index.js
@@ -0,0 +1,8 @@
+let addDays = require("date-fns/addDays");
+const getDays = (days) => {
+  const newDate = addDays(new Date(2020, 7, 22), days);
+  return `${newDate.getDate()}-${
+    newDate.getMonth() + 1
+  }-${newDate.getFullYear()}`;
+};
+module.exports = getDays;
